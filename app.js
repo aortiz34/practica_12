5172078568b6917f3161e55f755046851d192e6a
Axel Efraín Ortiz Reyes <aortiz@MacBook-Pro-de-Axel.local>
2022-04-05 21:29:34 -0500

    hola

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.get('/api', function(req, res) {
     res.json({firstname: 'John', lastname: 'Doe'});
 });
 
-app.get('/person/:id', function(req, res) {
-    res.render('person', { ID: req.params.id });
+app.get('/:id', function(req, res) {
+    res.render('number', { ID: req.params.id });
 });
 
 app.listen(port);
